c72c108291f3aaf43bbae81088e722bb689a6110
maram <maarram36@gmail.com>
2021-10-09 22:50:07 +0300

    project2

---
+++ jahez new/RestaurantMenuVC.swift
@@ -0,0 +1,19 @@
+//
+//  RestaurantMenuVC.swift
+//  jahez new
+//
+//  Created by Maram Al shahrani on 02/03/1443 AH.
+//
+
+import UIKit
+
+class RestaurantMenuVC : UIViewController{
+    
+    var restaurant : Restaurant?
+
+    
+    
+    @IBOutlet weak var menuTV: UITableView!
+    
+    
+}
---
+++ jahez new/jahez new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jahez new
+//
+//  Created by Maram Al shahrani on 02/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
