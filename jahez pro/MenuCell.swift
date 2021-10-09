e7378623c80c4b2999a2bc915512feea25f6154f
saja Al-qhtani <saja-alqhtani@sajas-MacBook-Air.local>
2021-10-09 23:53:31 +0300

    my project

---
+++ jahez pro/MenuCell.swift
@@ -0,0 +1,17 @@
+//
+//  MenueCell.swift
+//  jahez pro
+//
+//  Created by Hassan Yahya on 01/03/1443 AH.
+//
+
+import UIKit
+class MenuCell: UITableViewCell {
+    
+    @IBOutlet weak var restaurantimagemenucell: UIImageView!
+    
+    @IBOutlet weak var restaurantpricemenucell: UILabel!
+    
+    @IBOutlet weak var restaurantfoodmenucell: UILabel!
+    
+}
