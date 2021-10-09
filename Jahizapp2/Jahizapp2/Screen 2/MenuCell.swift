accdacaaad7d68606302fb229444b920ccf4e992
hanan asiri <kayan222276@yahoo.com>
2021-10-09 23:17:04 +0300

    project jahez

---
+++ Jahizapp2/Jahizapp2/Screen 2/MenuCell.swift
@@ -0,0 +1,30 @@
+//
+//  MenuCell.swift
+//  Jahizapp2
+//
+//  Created by  HANAN ASIRI on 29/02/1443 AH.
+//
+import UIKit
+import Foundation
+class MenuCell {
+    
+   
+    @IBOutlet var cell2: UIView!
+    
+    
+    @IBOutlet var name2: UILabel!
+    
+    
+    @IBOutlet var image2: UIImageView!
+    
+    
+    @IBOutlet var type2: UILabel!
+    
+    @IBOutlet var open2: UILabel!
+    
+    
+    @IBOutlet var rating2: UILabel!
+    
+    
+    
+}
---
+++ Jahizapp2/Jahizapp2/Screen 2/ResturantlistVC.swift
@@ -0,0 +1,8 @@
+//
+//  ResturantlistVC.swift
+//  Jahizapp2
+//
+//  Created by  HANAN ASIRI on 29/02/1443 AH.
+//
+
+import Foundation
