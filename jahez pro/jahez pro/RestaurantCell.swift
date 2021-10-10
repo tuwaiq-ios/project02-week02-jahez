c4a71845a1ed716f24b66372eed4863a196786b3
hasaan yahya <h.2011.h@hotmail.com>
2021-10-10 09:05:23 +0300

    not done

---
+++ jahez pro/jahez pro/RestaurantCell.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantCell.swift
+//  jahez pro
+//
+//  Created by Hassan Yahya on 29/02/1443 AH.
+//
+
+import UIKit
+class RestaurantCell: UITableViewCell {
+    
+    @IBOutlet weak var restaurantimagecell: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var type: UILabel!
+    
+    @IBOutlet weak var state: UILabel!
+    @IBOutlet weak var restaurantratingcell: UILabel!
+
+}
